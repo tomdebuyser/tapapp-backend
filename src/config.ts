e4fc1d1d5a35c9a94e86aa160cb644033ca46f2d
benno <benno.meysmans@icapps.com>
2020-01-02 15:41:15 +0100

    Add config

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,41 @@
-import { readFileSync } from 'fs';
+import { config } from 'dotenv-safe';
 import { join } from 'path';
 
-const rawConfig = readFileSync(join(__dirname, '../config.json')).toString();
-
-class Config {
-    private _config: Record<string, unknown>;
-
-    constructor(config: string) {
-        this._config = JSON.parse(config);
-    }
-
-    get port(): string {
-        return this._config.port as string;
-    }
-
-    get swaggerPath(): string {
-        return this._config.swaggerPath as string;
-    }
-
-    get projectName(): string {
-        return this._config.projectName as string;
+const environment = process.env.NODE_ENV;
+function assertNodeEnv(env: string | undefined): asserts env {
+    if (!env) {
+        throw Error('NODE ENV must be specified');
     }
 }
 
-export default new Config(rawConfig);
+assertNodeEnv(environment);
+
+const environmentsWithEnvFiles = ['local', 'testing'];
+if (environmentsWithEnvFiles.includes(environment)) {
+    const envFiles = {
+        example: join(__dirname, '../.env.example'),
+        testing: join(__dirname, '../.env.local'),
+        local: join(__dirname, '../.env.local'),
+    };
+
+    config({
+        path: envFiles[environment] || envFiles.local,
+        example: envFiles.example,
+    });
+}
+
+class Config {
+    static get port(): string {
+        return process.env.PORT as string;
+    }
+
+    static get swaggerPath(): string {
+        return process.env.SWAGGER_PATH as string;
+    }
+
+    static get projectName(): string {
+        return process.env.PROJECT_NAME as string;
+    }
+}
+
+export { Config };
--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,24 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
-import config from './config';
+import { Config } from './config';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
 
     setupSwagger(app);
 
-    await app.listen(config.port);
+    await app.listen(Config.port);
 }
 
 function setupSwagger(app: INestApplication): void {
     const options = new DocumentBuilder()
-        .setTitle(config.projectName)
+        .setTitle(Config.projectName)
         .setDescription('Swagger documentation')
         .setVersion('1.0')
         .build();
     const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup(config.swaggerPath, app, document);
+    SwaggerModule.setup(Config.swaggerPath, app, document);
 }
 
 bootstrap();
