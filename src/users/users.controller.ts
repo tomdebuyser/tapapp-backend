78acb6ca4dcbf2450ab4ffdfcf164e58bfa9e014
benno <benno.meysmans@icapps.com>
2020-01-03 17:18:34 +0100

    Move ts-node to dev dependancies

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Post, Body } from '@nestjs/common';
 
 import { CreateUserRequest } from './dto';
 import { UsersService } from './users.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
     constructor(private readonly userService: UsersService) {}
