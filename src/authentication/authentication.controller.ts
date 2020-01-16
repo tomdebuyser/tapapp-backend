266c52a2e3a4a95b045452e564f3252f0922c4ed
Tom De Buyser <tom.debuyser@icapps.com>
2020-01-16 12:32:10 +0000

    Merged in feature/SIL-70-authenticate (pull request #16)
    Feature/SIL-70 authenticate
    
    * fix pr comments
    
    * Merge branch 'feature/SIL-5-choose-password' of bitbucket.org:icapps/node_silvernext into feature/SIL-6-login
    
    * Merge branch 'feature/SIL-6-login' of bitbucket.org:icapps/node_silvernext into feature/redis
    
    * introduced session cookies stored in redis
    
    * add authenticate route
    
    * add tests
    
    * merge with develop
    
    * Merge branch 'feature/redis' of bitbucket.org:icapps/node_silvernext into feature/SIL-70-authenticate
    
    * merge with origin
    
    * Merge branch 'develop' of bitbucket.org:icapps/node_silvernext into feature/SIL-70-authenticate
    
    Approved-by: Benno Meysmans

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -5,6 +5,7 @@ import {
     UseGuards,
     HttpCode,
     HttpStatus,
+    Get,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
@@ -17,7 +18,7 @@ import { AuthenticationService } from './authentication.service';
 import { UserSession } from '../_shared/decorators/user-session.decorator';
 import { IUserSession } from '../_shared/constants';
 import { AuthenticationQueries } from './authentication.queries';
-import { LoginGuard } from '../_shared/guards';
+import { LoginGuard, AuthenticatedGuard } from '../_shared/guards';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -34,6 +35,14 @@ export class AuthenticationController {
         @UserSession() session: IUserSession,
         // body is not used, but here for swagger docs
         @Body() _body: LoginRequest,
+    ): Promise<LoggedInUserResponse> {
+        return this.authQueries.getLoggedInUser(session.userId);
+    }
+
+    @UseGuards(AuthenticatedGuard)
+    @Get('authenticate')
+    authenticate(
+        @UserSession() session: IUserSession,
     ): Promise<LoggedInUserResponse> {
         return this.authQueries.getLoggedInUser(session.userId);
     }
