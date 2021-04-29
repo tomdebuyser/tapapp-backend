8640ec50a623214639cff2be59c0eaa14859fbe6
Tom De Buyser <tom.debuyser@icapps.com>
2021-04-29 15:00:01 +0200

    update product entity

--- data/migrations/1619696414649-Product.ts
+++ data/migrations/1619696414649-Product.ts
@@ -5,14 +5,20 @@ export class Product1619696414649 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "product" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "createdBy" character varying, "updatedBy" character varying, "name" character varying NOT NULL, "logo" character varying NOT NULL, "price" integer NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "predecessorId" character varying, "organisationId" uuid, CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`,
+            `CREATE TABLE "product" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "createdBy" character varying, "updatedBy" character varying, "name" character varying NOT NULL, "logo" character varying NOT NULL, "price" integer NOT NULL, "isActive" boolean NOT NULL DEFAULT true, "predecessorId" uuid, "organisationId" uuid, CONSTRAINT "PK_bebc9158e480b949565b4dc7a82" PRIMARY KEY ("id"))`,
         );
         await queryRunner.query(
             `ALTER TABLE "product" ADD CONSTRAINT "FK_d905575f248a8d670b7f96abef4" FOREIGN KEY ("organisationId") REFERENCES "organisation"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
         );
+        await queryRunner.query(
+            `ALTER TABLE "product" ADD CONSTRAINT "FK_70bb52023aba5d4a6ddc610338a" FOREIGN KEY ("predecessorId") REFERENCES "product"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `ALTER TABLE "product" DROP CONSTRAINT "FK_70bb52023aba5d4a6ddc610338a"`,
+        );
         await queryRunner.query(
             `ALTER TABLE "product" DROP CONSTRAINT "FK_d905575f248a8d670b7f96abef4"`,
         );
--- libs/models/src/entities/product.entity.ts
+++ libs/models/src/entities/product.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, DeepPartial, ManyToOne } from 'typeorm';
+import {
+    Entity,
+    Column,
+    DeepPartial,
+    ManyToOne,
+    OneToOne,
+    JoinColumn,
+} from 'typeorm';
 import { plainToClass } from 'class-transformer';
 import { v4 as uuid } from 'uuid';
 
@@ -25,10 +32,10 @@ export class Product extends AuditedEntity {
     /**
      * This field represents the id of the product where this product is an update from.
      * We keep this information to be able to track product changes (e.g. price increase)
-     * We do not model this as a relationship as this could give potential DB issues.
      */
-    @Column({ nullable: true })
-    predecessorId?: string;
+    @OneToOne(() => Product, { nullable: true })
+    @JoinColumn()
+    predecessor?: Product;
 
     /** Relations */
 
