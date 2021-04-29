0de8d318f894549b5088a56781e3c34d0230714e
Tom De Buyser <tom.debuyser@icapps.com>
2021-04-29 21:44:38 +0200

    categories / products sorting

--- apps/api/src/categories/queries/get-categories.query.ts
+++ apps/api/src/categories/queries/get-categories.query.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
-import { CategoryRepository } from '@libs/models';
+import {
+    CategoryRepository,
+    CategorySortingRepository,
+    ProductSortingRepository,
+} from '@libs/models';
 import { IHandler } from '@libs/common';
 import { CategoryResponse } from '../dto';
 import { UserSession } from '../../shared/constants';
@@ -11,14 +15,19 @@ type GetCategoriesQuery = {
 
 @Injectable()
 export class GetCategoriesHandler implements IHandler<GetCategoriesQuery> {
-    constructor(private readonly categoryRepository: CategoryRepository) {}
+    constructor(
+        private readonly categoryRepository: CategoryRepository,
+        private readonly categorySortingRepository: CategorySortingRepository,
+        private readonly productSortingRepository: ProductSortingRepository,
+    ) {}
 
     async execute({
         userSession,
     }: GetCategoriesQuery): Promise<CategoryResponse[]> {
         const { organisation } = userSession;
 
-        return this.categoryRepository
+        // Get all the categories
+        const categories = await this.categoryRepository
             .createQueryBuilder('category')
             .select([
                 'category.id',
@@ -33,5 +42,40 @@ export class GetCategoriesHandler implements IHandler<GetCategoriesQuery> {
                 organisationId: organisation.id,
             })
             .getMany();
+
+        // Get the sorting of the categories
+        const categorySorting = await this.categorySortingRepository.findOne({
+            where: { organisation: { id: organisation.id } },
+        });
+
+        // Get the sorting of the products
+        const productsSorting = await this.productSortingRepository.find({
+            where: { organisation: { id: organisation.id } },
+            relations: ['category'],
+        });
+
+        // Return the sorted categories and products
+        const categoryIds =
+            categorySorting.categoryIds ||
+            categories.map(category => category.id);
+        return categories
+            .map(category => {
+                const productIds = productsSorting.find(
+                    sorting => sorting.category.id === category.id,
+                )?.productIds;
+                if (!productIds) return category;
+                return {
+                    ...category,
+                    products: category.products.sort(
+                        (p1, p2) =>
+                            productIds.indexOf(p1.id) -
+                            productIds.indexOf(p2.id),
+                    ),
+                };
+            })
+            .sort(
+                (c1, c2) =>
+                    categoryIds.indexOf(c1.id) - categoryIds.indexOf(c2.id),
+            );
     }
 }
